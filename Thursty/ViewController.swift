b775101237a765b3e6dfb2f8d166f798ea66247d
Siddharth Joshi <sjoshi804@gmail.com>
2018-07-07 10:46:20 +0530

    Initial Commit

---
+++ Thursty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thursty
+//
+//  Created by Siddharth Joshi on 7/7/18.
+//  Copyright © 2018 sjoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
