1caba8119712b317513b99eac3e9d223311581db
Siddharth Joshi <sjoshi804@gmail.com>
2018-07-19 00:10:10 +0530

    Get request working

--- Thursty/ViewController.swift
+++ Thursty/ViewController.swift
@@ -16,10 +16,44 @@ class ViewController: UIViewController
     @IBOutlet var partyLocation: UITextField!
     @IBOutlet var createPartyButton: UIButton!
     
-    @IBAction func createParty(sender: AnyObject)
+    struct Party: Decodable
     {
-        //Create new party hosted by user
+        let createdAt: String
+        let eventName: String
+        let hostedBy: String
+        let hostedByNameCache: String
+        let location: String
+        let partyid: String
+        let status: String
+        let time: String
+    }
+    
+    @IBAction func getPartyList(sender: AnyObject)
+    {
+        let getURL = URL(string: "https://test-thursday.herokuapp.com/party/all/")!
+        var getRequest = URLRequest(url: getURL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 30.0)
+        getRequest.httpMethod = "GET"
+        getRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        getRequest.setValue("application/json", forHTTPHeaderField: "Accept")
         
+        URLSession.shared.dataTask(with: getRequest, completionHandler: { (data, response, error) -> Void in
+            if error != nil { print("GET Request: Communication error: \(error!)") }
+            if data != nil {
+                do {
+                    let resultObject = try JSONSerialization.jsonObject(with: data!, options: [])
+                    DispatchQueue.main.async(execute: {
+                        print("Results from GET https://httpbin.org/get?bar=foo :\n\(resultObject)") })
+                } catch {
+                    DispatchQueue.main.async(execute: {
+                        print("Unable to parse JSON response")
+                    })
+                }
+            } else {
+                DispatchQueue.main.async(execute: {
+                    print("Received empty response.")
+                })
+            }
+        }).resume()
     }
 }
 
